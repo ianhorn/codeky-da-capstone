b2a518ef6ee1c8e066c7456b2cfa244d4856c3ff
ianhorn <ian.horn@ky.gov>
2025-03-16 23:42:29 -0400

    fill depressions function

---
+++ project/scripts/constants.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+from whitebox_workflows import WbEnvironment
+from IPython.display import display
+
+# create a data from for the scenes
+def create_scenes_df():
+    
+    """
+        These files were obtained using the ASF Vertex Data Search.
+        They were submitted for Radio Terrain Correctiom.
+        radiometry: gamma0
+        scale: decibel
+        pixel spacing: 10m
+    """
+
+    box_data = {
+        'zipfile': ['S1A_IW_20250205T233956_DVP_RTC10_G_gdufem_246A.zip',
+                    'S1A_IW_20250217T233955_DVP_RTC10_G_gdufem_E701.zip'],
+        'link': ['https://ky.box.com/shared/static/xwhzpb6entefdhsi8jfhgii1f4ehh6x6.zip', 
+                'https://ky.box.com/shared/static/81wf3fabzhzsux29nhe4ojdaq0qtwp89.zip']
+            }
+    scenes_df = pd.DataFrame(data=box_data)
+
+    # calculate some fields
+    scenes_df['scene_folder'] = scenes_df['zipfile'].apply(lambda x: os.path.splitext(x)[0])
+    scenes_df['input_dem'] = scenes_df['scene_folder'] + '_dem.tif'
+    scenes_df['rgb'] = scenes_df['scene_folder'] +  '_rgb.png'
+    scenes_df['vv'] = scenes_df['scene_folder'] + '_vv.tif'
+    scenes_df['vh'] = scenes_df['scene_folder'] + '_vh.tif'
+    scenes_df['shapefile'] = scenes_df['scene_folder'] + '_shape.shp'
+
+    # print an example
+    # print(scenes_df)
+
+    # base_scene = scenes_df.at[0,'scene_folder']
+    # flood_scene = scenes_df.at[1,'scene_folder']
+
+    # print(f'Base Scene Directory:  {base_scene}\nFlood Scene Directory: {flood_scene}')
+    
+    return scenes_df
+
+# setup Whitebox Workflows envrionment
+wbe = WbEnvironment()
+
+# fill remove depressions in the dem
+def dem_fill(file, temp_dir):
+    
+    """
+    the `wbe.fill_depressions` function is used to fill any voids and anomolies in the dem
+    """
+
+    wbe.verbose = True
+    wbe.work_directory = temp_dir
+    print(f'Working directory: {wbe.work_directory}')
+
+    # set up so that you don't have to do the fill depressions process if already in memory
+    # I don't think this will actually work
+    try:
+        input_dem_raster
+    except NameError:
+        input_dem_raster = wbe.read_raster(file)
+        print(input_dem_raster.file_name)
+        display("Starting DEM filling process...")
+        filled_dem_raster = wbe.fill_depressions(input_dem_raster)  # leave default parameter, recommended by documetation
+        display("DEM filling complete.")
+            # write to file
+        output_file = os.path.join(temp_dir, 'filled_dem.tif')
+        filled_dem_raster.wbe_write(filled_dem_raster, output_file)
+    else:
+        output_file = os.path.join(temp_dir, 'filled_dem.tif')
+        filled_dem_raster = wbe.write_raster(filled_dem_raster, output_file)
+   
+    return output_file
